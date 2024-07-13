d90e478b91113abf4de0b5b6a07e9db2ced6f11c
Komal Ramapragada <komalramapragada@Komals-iMac.local>
2024-07-13 18:10:18 +0530

    Commiting functional Components

--- first/App.js
+++ first/App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 //const heading = React.createElement("h1" , {id : "heading"} , "This is a React Element");
+const Name = () =>(
+    <>
+    <h2>Sova</h2>
+    </>
+);
 
-const jsxheading = <h1 id="heading">JSX heading</h1> ; 
+
+
+
+const Heading = () =>(
+    <div>
+    <Name/>
+    <h1>I AM THE HUNTER!!!!</h1>
+    </div>
+);
+
 
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<Heading/>);
+
+
