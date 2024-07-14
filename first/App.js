d42ffb7eb4fb36c192dd2c83e3a98ae66c813889
Komal Ramapragada <komalramapragada@Komals-iMac.local>
2024-07-14 12:53:59 +0530

    Hard coded Restro -card

--- first/App.js
+++ first/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import  ReactDOM  from 'react-dom/client';
 
-//const heading = React.createElement("h1" , {id : "heading"} , "This is a React Element");
-const Name = () =>(
-    <>
-    <h2>Sova</h2>
-    </>
-);
+/* Components
+ Header
+   - Logo
+   - Nav Items 
+ Body
+   - Search
+   - Restaurant Container
+       # RestaurantCards
+ Footer
+   - Copyright
+   - Links
+   - Info... etc.
+
+
+*/
+const Header = () =>{
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://png.pngtree.com/png-clipart/20220628/original/pngtree-food-logo-png-image_8239850.png'/>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+const RestaurantCard = () =>{
+    return(
+        <div className='res-card'>
+            <img className='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ne98mz09ikwfpz3ndws6'/>
+            <h3>Meghna Foods</h3>
+            <h4>Biryani , Indian</h4>
+            <h4>4.5 stars</h4>
+            <h4>38 mins</h4>
+        </div>
+    );
+}
+
+const Body = () =>{
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+
+            </div>
+        </div>
+    );
+}
+
+const Applayout = () =>{
+    return (
+        <div className='app'>
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
 
 
 
-
-const Heading = () =>(
-    <div>
-    <Name/>
-    <h1>I AM THE HUNTER!!!!</h1>
-    </div>
-);
 
 
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading/>);
+root.render(<Applayout/>);
 
 
