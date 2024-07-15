efa349611ca05c8f5e4ef65f03d5cd286875faf1
Komal Ramapragada <komalramapragada@Komals-iMac.local>
2024-07-15 19:18:56 +0530

    Made a real time API call using swiggy API.. started using {useEffect}

--- first/src/components/Body.js
+++ first/src/components/Body.js
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import {useState} from "react";
+import {useState , useEffect} from "react";
 
 
 
@@ -8,6 +8,22 @@ const Body = () =>{
 
    const [Restro , setRestro] = useState(resList);
 
+   useEffect(()=>{
+    fetchData();
+   },[]);
+
+   const fetchData = async () =>{
+    const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4523764&lng=78.5335018&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+
+        console.log(json);
+
+        setRestro(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+   };
+
+  
 
     return (
         <div className='body'>
